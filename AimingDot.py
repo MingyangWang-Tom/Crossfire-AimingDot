65ad27601667836798f05a541405e7bd6864c955
MingyangWang-Tom <mingyang.wang97@gmail.com>
2023-10-05 21:21:49 -1000

    hide aiming dot when hold right-click (beta)

--- AimingDot.py
+++ AimingDot.py
@@ -1,5 +1,6 @@
 import tkinter as tk
 from tkinter import colorchooser
+from pynput import mouse
 
 class App:
     def __init__(self):
@@ -7,6 +8,7 @@ class App:
         self.root.title("Aiming Dot Controller")
 
         self.dot_window = None  # Reference to the dot window
+        self.mouse_listener = None
 
         self.dot_color = tk.StringVar(value="#FF0000")
         self.dot_x = tk.IntVar(value=self.root.winfo_screenwidth() // 2)
@@ -26,6 +28,11 @@ class App:
         tk.Label(self.root, textvariable=self.dot_color).pack(pady=5)
 
         tk.Button(self.root, text="Show Dot", command=self.show_dot).pack(pady=10)
+
+        # Toggle right-click behavior button
+        self.toggle_btn = tk.Button(self.root, text="Enable Right-Click Hide", command=self.toggle_mouse_listener)
+        self.toggle_btn.pack(pady=10)
+
         tk.Button(self.root, text="Exit", command=self.root.quit).pack(pady=10)
 
         self.root.mainloop()
@@ -50,6 +57,30 @@ class App:
         canvas = tk.Canvas(self.dot_window, width=size, height=size, bg="white", bd=0, highlightthickness=0)
         canvas.pack()
         canvas.create_oval(0, 0, size, size, fill=self.dot_color.get())
+
+    def toggle_mouse_listener(self):
+        if self.mouse_listener and self.mouse_listener.running:
+            self.mouse_listener.stop()
+            self.toggle_btn.config(text="Enable Right-Click Hide")
+        else:
+            self.mouse_listener = mouse.Listener(on_click=self.handle_click)
+            self.mouse_listener.start()
+            self.toggle_btn.config(text="Disable Right-Click Hide")
+
+    def handle_click(self, x, y, button, pressed):
+        if button == mouse.Button.right:
+            if pressed:
+                self.hide_dot()
+            else:
+                self.reveal_dot()
+
+    def hide_dot(self):
+        if self.dot_window:
+            self.dot_window.withdraw()
+
+    def reveal_dot(self):
+        if self.dot_window:
+            self.dot_window.deiconify()
 
 if __name__ == "__main__":
     App()
